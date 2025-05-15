4661b1b6b4fa0aea996137a4b76fad1fd0b1bd15
Muhammad Baqir <mbaqir856@gmail.com>
2025-05-14 17:40:24 -0700

    Complete 12, 13 exercises

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	arr.forEach((num) => sum += num);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    total *= arr[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+  return num * (factorial(num - 1));
 };
 
 // Do not edit below this line
--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    const reversed = string.split("").reverse().join("");
+    return string === reversed;
 };
 
 // Do not edit below this line
