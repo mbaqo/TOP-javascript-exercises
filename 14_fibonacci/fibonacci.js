56b21191440b16a25dfdf19af32a8fbe2434b063
Muhammad Baqir <mbaqir856@gmail.com>
2025-05-14 21:02:13 -0700

    Complete 14, 15, 16 exercises

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = Number(index);
 
+    if (index < 0) {
+        return "OOPS";
+    }
+    if (index === 0) {
+        return 0;
+    }
+
+    let first = 0;
+    let second = 1;
+    for (let i = 2; i <= index; i++) {
+        let current = first + second;
+        first = second;
+        second = current;
+    }
+
+    return second;
 };
 
 // Do not edit below this line
