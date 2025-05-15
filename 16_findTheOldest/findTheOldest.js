4b802674d27baa83dcc2d1aff8d14045650c4d1e
Muhammad Baqir <mbaqir856@gmail.com>
2025-05-14 22:13:18 -0700

    Complete 16 exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
 const findTheOldest = function(people) {
-    
+    return people.map ((person) => {
+        const age = person.yearOfDeath
+        ? person.yearOfDeath - person.yearOfBirth
+        : new Date().getFullYear() - person.yearOfBirth;
+        return {...person, age: age};
+    }).reduce ((oldest, person) => {
+        if (oldest.age < person.age) {
+            return person;
+        }
+        return oldest;
+    })
 };
 
 // Do not edit below this line
